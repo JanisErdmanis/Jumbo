8639ec905deefa36e172ebd360ca8afc9ee0c884
Janis Erdmanis <janiserdmanis@protonmail.ch>
2026-03-27 20:42:59 +0200

    updating startup.jl

--- meta/startup.jl
+++ meta/startup.jl
@@ -9,6 +9,15 @@
 # The diagnostic output below shows the active project, load paths, and depot paths
 # to help verify your environment configuration.
 
+if isdir(joinpath(last(DEPOT_PATH), "compiled/v$(VERSION.major).$(VERSION.minor)", "AppEnv")) || any(i -> i.name == "AppEnv", keys(Base.loaded_modules))
+    import AppEnv
+else
+    include(joinpath(Sys.STDLIB, "AppEnv/src/AppEnv.jl"))
+end
+AppEnv.init()
+
+Base.ACTIVE_PROJECT[] = AppEnv.USER_DATA
+
 if isinteractive()
     @async begin
         @eval using Revise
